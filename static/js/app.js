7e905e237d748eb56ba2a84938a6092a4ac5dbbd
anirudhi89 <anirudh.iyer@outlook.com>
2022-11-10 17:24:35 -0500

    new file creation

---
+++ static/js/app.js
@@ -0,0 +1,29 @@
+var experiencesel = document.getElementById('experience');
+
+var tst = document.getElementById('tst');
+
+
+experiencesel.onload = function() {
+    // tst.innerText = experiencesel.value;
+    getExperiencesList(experiencesel.value);
+}
+experiencesel.onchange = function() {
+    // tst.innerText = experiencesel.value;
+    getExperiencesList(experiencesel.value);
+}
+
+function getExperiencesList(value) {
+    document.getElementById('ddn').hidden = true;
+    document.getElementById('upmc').hidden = true;
+    document.getElementById('swanson').hidden = true;
+    document.getElementById(value).hidden = false;
+}
+
+
+// show fullscreen noti
+
+window.addEventListener("load", (event) => {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+})
